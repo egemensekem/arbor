955ae8232d6fad68c7654d32dff98ec222aa84e2
egemensekem <egemen.sekem@mebitech.com>
2018-03-13 10:56:42 +0300

    update

--- src/main/java/com/arbor/App.java
+++ src/main/java/com/arbor/App.java
@@ -2,14 +2,13 @@ package com.arbor;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
